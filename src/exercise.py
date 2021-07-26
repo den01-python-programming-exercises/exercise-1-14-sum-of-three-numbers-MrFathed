aadb4dea45de9763ec7c132f66ba5c9d04ff0f4f
Justin Turner <jmturner638@gmail.com>
2021-07-26 02:59:52 -0500

    Second Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,8 +1,8 @@
 def main():
     #write your code below this line
-    first = int(input("Give me the first number:"))
-    second = int(input("Give me the second number:"))
-    third = int(input("Give me the third number:"))
+    first = int(input("Give the first number:"))
+    second = int(input("Give the second number:"))
+    third = int(input("Give the third number:"))
 
     print("The sum of the numbers is " + str(first + second + third))
 
