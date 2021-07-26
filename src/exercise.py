e407f44e3b29d0f2efbe5dc318d44982d5c9a78a
Justin Turner <jmturner638@gmail.com>
2021-07-26 02:58:10 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,10 @@
 def main():
     #write your code below this line
+    first = int(input("Give me the first number:"))
+    second = int(input("Give me the second number:"))
+    third = int(input("Give me the third number:"))
+
+    print("The sum of the numbers is " + str(first + second + third))
 
 if __name__ == '__main__':
     main()
